adb84365ff77a3b4d675824797775e7b86e0822b
Malak37 <71739461+Malak37@users.noreply.github.com>
2022-05-10 12:27:34 -0400

    commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+​
+app.use(express.static(__dirname + '/public'));
+io.on('connection', onConnection);
+http.listen(port, () => console.log('listening on port ' + port));
+
+const numRooms = 5;
+const maxPeople = 4;

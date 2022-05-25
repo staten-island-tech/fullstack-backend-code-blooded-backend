534d9ddb1ca9f1e689edf2eb0cadff71bc9c8923
Malak37 <71739461+Malak37@users.noreply.github.com>
2022-05-25 01:31:09 -0400

    commit

--- server.js
+++ server.js
@@ -1,126 +1,78 @@
 const express = require("express");
+const socketio = require("socket.io");
 const http = require("http");
-const { Server, Socket } = require("socket.io");
+const cors = require("cors");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+const io = socketio(server);
 
-let rooms = [];
-let roomsInfo = [];
+app.use(cors());
 
 io.on("connection", (socket) => {
-  // index of the room in "rooms"
-  let hostRoomIndex = null;
+  socket.on("join", (payload, callback) => {
+    let numberOfUsersInRoom = getUsersInRoom(payload.room).length;
 
-  let imHost = null;
-  let roomInfoIndex = null;
-  let myRoomCode = null;
+    const { error, newUser } = addUser({
+      id: socket.id,
+      name: numberOfUsersInRoom === 0 ? "Player 1" : "Player 2",
+      room: payload.room,
+    });
 
-  // for the guest
-  let guestInfoIndex = null;
+    if (error) return callback(error);
 
-  console.log(`user ${socket.id} is connected`);
-  console.log(`user is connected`);
+    socket.join(newUser.room);
 
-  // placing the host that is
-  socket.on("placeHost", (username, code, hostStatus) => {
-    // sets host status
-    imHost = hostStatus;
-    myRoomCode = code;
-
-    // joining and pushing room code into room
-    socket.join(code);
-    rooms.push(code);
-
-    // pushing array with username into room info
-    let addRoomInfo = [username];
-    roomsInfo.push(addRoomInfo);
-
-    // index of the room in "rooms"
-    hostRoomIndex = rooms.lastIndexOf(code);
-    // index of the array of usernames (specific room) in roomInfo
-    roomInfoIndex = roomsInfo.lastIndexOf(addRoomInfo);
-
-    console.log(
-      "these are the rooms: " + rooms + "all the room info: " + roomsInfo
-    );
-
-    io.to(code).emit("currentRoom", roomsInfo[roomInfoIndex]);
+    io.to(newUser.room).emit("roomData", {
+      room: newUser.room,
+      users: getUsersInRoom(newUser.room),
+    });
+    socket.emit("currentUserData", { name: newUser.name });
+    callback();
   });
 
-  // time to check room existence woahh
-  socket.on("checkRoom", (arg) => {
-    let verified = rooms.includes(arg);
-
-    let myRoomIndex = rooms.indexOf(myRoomCode) + 1;
-    // console.log(myRoomIndex);
-    let thisRoom = roomsInfo[myRoomIndex];
-    let full = thisRoom.length > 3;
-
-    socket.emit("checked", verified, full);
+  socket.on("initGameState", (gameState) => {
+    const user = getUser(socket.id);
+    if (user) io.to(user.room).emit("initGameState", gameState);
   });
 
-  // placing the guest in a room
-  socket.on("placeGuest", (username, code, hostStatus) => {
-    imHost = hostStatus;
-    myRoomCode = code;
-
-    socket.join(code);
-
-    // which room u in
-    hostRoomIndex = rooms.indexOf(code);
-
-    // adding roominfo to roominfo
-    roomsInfo[hostRoomIndex].push(username);
-
-    // in the array of usernames in a room, position of the guest username
-    let myRoom = roomsInfo[hostRoomIndex];
-    guestInfoIndex = myRoom.length - 1;
-
-    console.log(
-      " //these are the rooms:// " +
-        rooms +
-        " //all the room info:// " +
-        roomsInfo
-    );
-    io.to(code).emit("currentRoom", roomsInfo[hostRoomIndex]);
+  socket.on("updateGameState", (gameState) => {
+    const user = getUser(socket.id);
+    if (user) io.to(user.room).emit("updateGameState", gameState);
   });
 
-  // chat mech here
-  socket.on("myMessage", (message, code) => {
-    io.to(code).emit("newMessage", message);
+  socket.on("sendMessage", (payload, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", {
+      user: user.name,
+      text: payload.message,
+    });
+    callback();
   });
 
   socket.on("disconnect", () => {
-    console.log(`user ${socket.id} left.`);
-
-    if (imHost === true) {
-      let myRoomIndex = rooms.indexOf(myRoomCode);
-      rooms.splice(myRoomIndex, 1);
-      roomsInfo.splice(myRoomIndex, 1);
-    } else {
-      if (rooms.includes(myRoomCode)) {
-        let myRoomIndex = rooms.indexOf(myRoomCode);
-        roomsInfo[myRoomIndex].splice(guestInfoIndex, 1);
-        console.log("testing" + roomsInfo);
-      }
-    }
-
-    console.log(
-      " //these are the rooms:// " +
-        rooms +
-        " //all the room info:// " +
-        roomsInfo
-    );
+    const user = removeUser(socket.id);
+    if (user)
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
   });
 });
 
-server.listen(3001, () => {
-  console.group("Server is running on 3001");
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
